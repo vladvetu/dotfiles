21196837ee3979ef2638d20b3b75eab309ff2a6b
Vlad Vetu <vetu.vlad@gmail.com>
2022-02-12 13:32:18 +0200

    Add private_dot_config/zsh/alias.d/chezmoi.zsh

---
+++ private_dot_config/zsh/alias.d/chezmoi.zsh
@@ -0,0 +1,7 @@
+#alias cz ='chezmoi'
+
+alias cz-add='chezmoi add'
+alias cz-edit='chezmoi edit'
+alias cz-test ='chezmoi apply -vn'
+alias cz-apply ='chezmoi apply -v'
+alias cz-update ='chezmoi update'
